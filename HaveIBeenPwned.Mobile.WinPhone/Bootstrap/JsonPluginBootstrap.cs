e6f4e6951bcd38299462fac60f96a1e5f3056dcb
ChrisWay <chris@darksquare.net>
2014-04-12 18:59:57 +0100

    WInRT + WinPhone version

---
+++ HaveIBeenPwned.Mobile.WinPhone/Bootstrap/JsonPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using Cirrious.CrossCore.Plugins;
+
+namespace HaveIBeenPwned.Mobile.WinPhone.Bootstrap
+{
+    public class JsonPluginBootstrap
+        : MvxPluginBootstrapAction<Cirrious.MvvmCross.Plugins.Json.PluginLoader>
+    {
+    }
+}
